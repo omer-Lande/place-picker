0486c268fd9c7418998c5c95589b3f3aebc932f6
Omer Lande <omerl@log-on.com>
2025-03-31 17:44:11 +0300

    added put command for places we want to visit

--- src/http.js
+++ src/http.js
@@ -8,3 +8,21 @@ export async function fetchAvailablePlaces() {
 
   return resData.places;
 }
+
+export async function updateUserPlaces(places) {
+  const respone = await fetch("http://localhost:3000/user-places", {
+    method: "PUT",
+    body: JSON.stringify({ places: places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const resData = await respone.json();
+
+  if (!respone.ok) {
+    throw new Error("Failed To Update User Data");
+  }
+
+  return resData.message;
+}
